f420987de4c41ac1aa73f6854f6a2073c1862c16
satishiyer31 <satishiyer31@gmail.com>
2022-03-13 18:48:35 -0600

    Moved cors to top

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
-app.use(cors());
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
